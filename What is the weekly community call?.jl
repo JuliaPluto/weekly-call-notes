84199e2efe46278145265c8b840163238525a900
Fons van der Plas <fonsvdplas@gmail.com>
2022-04-04 13:18:24 +0200

    Create What is the weekly community call?.jl

---
+++ What is the weekly community call?.jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.18.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c6d4a34e-b407-11ec-1c9c-bf41c71557b5
+md"""
+# Weekly Pluto community call!
+
+This is the weekly community call for Pluto! We discuss what happened in Pluto-land during the past week, what people are working on, and what we are planning for the future. 
+
+The call is *open*, which means that everyone is welcome to join, also if you just want to listen. **Feel free to join!** (You do no need to work on Pluto's internal codebase to join! Most of us don't!)
+
+![](https://raw.githubusercontent.com/gist/fonsp/9a36c183e2cad7c8fc30290ec95eb104/raw/ca3a38a61f95cd58d79d00b663a3c114d21e284e/cute.svg)
+
+## Links
+
+📝 The notes from previous talks can be read here (on this website):
+[https://juliapluto.github.io/weekly-call-notes/](https://juliapluto.github.io/weekly-call-notes/)
+
+💬 For more information, check out the Pluto Developers Zulip: [https://gist.github.com/fonsp/db7d00fd3fe5bc0b379b4af9ec6674b6](https://gist.github.com/fonsp/db7d00fd3fe5bc0b379b4af9ec6674b6)
+
+🎥 Here is a recording of a previous developer call, to get an idea of what it is like! [https://www.youtube.com/watch?v=FO5jLHlFWQg](https://www.youtube.com/watch?v=FO5jLHlFWQg)
+
+
+## When is the next meeting?
+
+Our meetings are scheduled on the [Julia Community Events calendar](https://julialang.org/community/#events), go there to find the next date!
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─c6d4a34e-b407-11ec-1c9c-bf41c71557b5
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
