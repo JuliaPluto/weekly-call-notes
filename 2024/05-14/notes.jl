dfe06052cf432394520714cd3afc20d5d12953a6
Fons van der Plas <fonsvdplas@gmail.com>
2024-05-14 16:47:36 +0200

    Create notes.jl

---
+++ 2024/05-14/notes.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e69a1bd8-11f6-11ef-2430-83d2ae8a2a95
+md"""
+# Weekly call notes 14 may 2024
+
+Hello!! Welcome back!
+
+Recently fons has been working on lots of things but we haven't kept this site will call notes up to date. Sorry! But check Pluto's release notes and new content on [https://plutojl.org/en/docs/](https://plutojl.org/en/docs/)
+"""
+
+# ╔═╡ ae1a243d-a809-498e-82f2-82b5ea4bf87c
+md"""
+# Segio: updates to lezer and more
+
+Sergio is now the maintainer of [tree sitter Julia grammar](https://github.com/tree-sitter/tree-sitter-julia) for some while and now he can bring all the goodies to lezer Julia!
+
+[https://github.com/JuliaPluto/lezer-julia/pull/7](https://github.com/JuliaPluto/lezer-julia/pull/7)
+
+most codemirror-related issues on Pluto will be fixed by this! (by accident hihi)
+"""
+
+# ╔═╡ Cell order:
+# ╟─e69a1bd8-11f6-11ef-2430-83d2ae8a2a95
+# ╟─ae1a243d-a809-498e-82f2-82b5ea4bf87c
